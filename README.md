6c9459b33e443aefe6cad9007b6a92fbc047d15d
Mark Feeney <mark.feeney@gmail.com>
2015-02-11 23:48:38 -0500

    Move docs to readme, parameterize penalty

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 # hhco
 
-Hi Ho! Cherro-O stats.  Really.
+![](docs/mickey.jpg)
+
+## wtf
+
+Figure out the rules and expected number of turns to win the terrible kids'
+game "HiHo!  Cherry-O Mickey Mouse Clubhouse Edition"
+
+[Don't buy.](http://www.amazon.com/Cherry-O-Disney-Mickey-Clubhouse-Edition/dp/B00IFWSO8K)
+
+Why? Because the rules supplied are inscrutable, so the only rational path
+forward is to write a simple simulation to test alternatives.
+
+Said differently, I did this to answer the question **can this game really
+take this long???**
+
+## game
+
+Each player starts with a tree with ten fruits on it.  The object of the game
+is to move all your fruit to a picnic area.  You move fruit by spinning a wheel
+and moving the prescribed number of fruit.
+
+The spinner has seven equally likely spots, each dictating how many fruits to
+move from the current player's tree to the picnic. Probably? There are many
+interpretations of the spinner, but the least ridiculous one seems to be to
+count the number of fruits on the spot, and ignore the numbers and colours.
+
+One of the spots, "Pluto", causes all players to remove three fruits from the
+picnic and put them back on their respective trees. Potentially? Or maybe it's
+one fruit each for a total of three. Only God and Hasbro know for sure, though
+I endeavour to find out.
+
+Simple EV (in fruits moving from tree->picnic) of a spin is:
+(1+1+2+2+3+3-3)/7 == 9/7 == 1.285714286 fruit/spin
+
+For a 1 player game you might expect 10/(9/7) == 7.778 spins. Problem is that
+negative valued spins before you have sufficient fruits at the picnic don't
+hurt you as badly, so "experimentally" the expected number of spins to win a
+single player game seems to be around 7.59:
+
+    (hhco.core/-main 100000 1) ;7.58982
+
+With three players, you've got more people rolling negatives and prolonging
+the game. Expect the winner to take ~9.5 spins in this case.
+
+    (hhco.core/-main 100000 3) ;9.49713
+
+This is the number of spins for *the winner*, so for three players we're
+looking at ~28.5 spins, which is an eternity for kids. From this I suspect the
+Pluto rule is really each player puts one fruit back on the tree.  If that's
+the case, the winner wins in ~6.14 turns:
+
+    (hhco.core/-main 100000 3 -1) ;6.14403
+
+This is moderately more tolerable.
+
+I suspect the math for the exact solution to these questions might be easier
+than I think, but I am terrible at probability and wanted to write a program,
+which is guaranteed to be bug free.  If you happen to know how to get the exact
+solution (i.e. without monte carlo methods), let me know!
 
--- src/hhco/core.clj
+++ src/hhco/core.clj
@@ -1,95 +1,51 @@
 (ns hhco.core
-  "Figure out expected number of turns to win terrible kids game 'HiHo!
-  Cherry-O Mickey Mouse Clubhouse Edition'
-
-  Don't buy: http://www.amazon.com/Cherry-O-Disney-Mickey-Clubhouse-Edition/dp/B00IFWSO8K
-
-  Why? Because the rules supplied are inscrutable, so the only rational path
-  forward is to write a simulation in an obscure programming language to to
-  test alternatives.
-
-  Said differently, I did this to answer the question 'can this game really
-  take this long???'
-
-  The spinner has seven equally likely spots, each dictating how many apples to
-  move from the current player's tree to the picnic. Probably. There are many
-  interpretations of the spinner, but the least ridiculous one seems to be to
-  count the number of fruits on the spot, and ignore the numbers and colours.
-
-  One of the spots, 'Pluto',  causes all players to remove 3 apples from the
-  picnic and put them back on their respective trees.  Potentially.  Or maybe
-  it's one apple each for a total of 3.  Only God and Hasbro know for sure.
-
-  Simple EV (in apples moving from tree->picnic) of a spin is:
-  (1+1+2+2+3+3-3)/7 == 9/7 == 1.285714286
-
-  For a 1 player game you might expect 10/(9/7) == 7.778 spins. Problem is that
-  negative valued spins before you have sufficient apples at the picnic don't
-  hurt you as badly, so 'experimentally' the expected number of spins to win a
-  1 player game seems to be around 7.59:
-
-  (hhco.core/-main 100000 1) ;7.58982
-
-  With three players, you've got more people rolling negatives and prolonging
-  the game.  Expect the winner to take ~9.5 spins in this case.
-
-  (hhco.core/-main 100000 3) ;9.49713
-
-  This is the number of spins for *the winner*, so for three players we're
-  looking at ~28.5 spins, which is an eternity for kids. I suspect I have
-  the Pluto rule wrong.
-
-  I fear the real math for this might be easier than I think, but I am terrible
-  at probability and wanted to write a program, which is guaranteed to be bug
-  free.
-  ")
+  "See README.md")
 
 (set! *unchecked-math* true)
 (set! *warn-on-reflection* true)
 
 (def TREE_CAPACITY 10)
 
-(defn spin
-  "Returns the number of apples to move from tree -> picnic.  Can be negative,
-  meaning the apples move from picnic to tree.
-  "
-  []
-  (case (int (rand-int 7))
-    0 1
-    1 1
-    2 2
-    3 2
-    4 3
-    5 3
-    6 -3))
+(defn make-spinner
+  "Makes a 0-arg spinner function with given penalty.  Returned function
+  returns the number of fruits to move from tree -> picnic.  Can be negative,
+  meaning the fruits move from picnic to tree."
+  [penalty]
+  (fn []
+    (case (int (rand-int 7))
+      0 1
+      1 1
+      2 2
+      3 2
+      4 3
+      5 3
+      6 penalty)))
 
 (defn do-turn
   "Spin and update gamestate for player p.  Return new gamestate."
-  [p g]
-  (let [spin (spin)
-        ;_ (prn p spin)
-        move-apples #(-> (- % spin)
+  [p g spin]
+  (let [move-fruit #(-> (- % spin)
                          (min TREE_CAPACITY)
                          (max 0))
         g (cond-> g
             true (update-in [p :spins] inc)
-            (pos? spin) (update-in [p :tree] move-apples))
+            (pos? spin) (update-in [p :tree] move-fruit))
         g (if (neg? spin)
-            ;; put spin apples back on everyone's tree, if possible
+            ;; put spin fruits back on everyone's tree, if possible
             (reduce-kv (fn [acc player pstate]
                          (assoc acc
                                 player
-                                (update-in pstate [:tree] move-apples)))
+                                (update-in pstate [:tree] move-fruit)))
                        {}
                        g)
             g)]
     g))
 
 (defn turns-to-win
-  [num-players]
+  [num-players spinner]
   (let [g (zipmap (range num-players) (repeat {:tree TREE_CAPACITY :spins 0}))]
     (loop [p 0 turn 1 g g]
-      (let [g' (do-turn p g)]
+      (let [g' (do-turn p g (spinner))]
         (if (zero? (get-in g' [p :tree]))
           [p (get-in g' [p :spins])]
           (recur (long (mod (inc p) num-players))
@@ -98,10 +54,12 @@
 
 (defn -main [& args]
   (let [num-games (or (first args) 10000)
-        num-players (or (second args) 3)]
+        num-players (or (second args) 3)
+        penalty (nth args 2 -3)
+        spinner (make-spinner penalty)]
     (loop [i 0
            sum 0]
-      (let [[_ num-turns] (turns-to-win num-players)]
+      (let [[_ num-turns] (turns-to-win num-players spinner)]
         (if (< i num-games)
           (recur (inc i) (long (+ sum num-turns)))
           (/ sum (double i)))))))
